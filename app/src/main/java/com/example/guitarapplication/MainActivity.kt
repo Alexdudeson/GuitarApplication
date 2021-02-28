f49c8e81b2a255738f802f94b76b363da37a813e
Alexey <alexxd91@gmail.com>
2021-02-28 18:39:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/guitarapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.guitarapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+
+class MainActivity : AppCompatActivity()
+{
+
+      // public class GuitarBrands ()
+
+    override
+    fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+      // на потом
+      //  ArrayAdapter guitarBrandsAdaper = new ArrayAdapter
+      // (this, android.R.layout.simple_spinner_item, GuitarBrands);
+      //   GuitarBrandsAdaper.setDropDownViewResource
+      //  (android.R.layout.simple_spinner_dropdown_item_item)
+
+
+    }
+}
